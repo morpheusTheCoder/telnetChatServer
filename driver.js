5f0af92ce263e4560df4f867cd416f5d917fd0a4
chinmay_netseer <chinmay.parekh@netseer.com>
2015-11-01 16:53:16 -0800

    cleanup and minor bug fixes

--- driver.js
+++ driver.js
@@ -12,163 +12,16 @@ var userNameToRoomNameMap = {};
 var roomNameToAudienceSocketArrayMap = {};
 
 
-
+// TODO: make commands case insensitive
+// TODO: detect invalid commands and print error message
+// TODO: 
 
 
 // TODO: Check if I use the same client machine to login again with a different user name , it honours new name.
 // TODO: Check even if there is a single user in system, that user should be able to join any room.
 // TODO: Modularize the code
 
-
-
-
-
-
-
-
-
-
-
-function getUserNameForSocket(socket){
-   return socketNameToUserNameMap['mysocket.' + socketArray.indexOf(socket)] ;
-}
-
-
-
-// Cleans the input of carriage return, newline
-function cleanInput(data) {
-  return data.toString().replace(/(\r\n|\n|\r)/gm,"");
-}
- 
-
-
-
-function quit(socket){
-  socket.end('Goodbye!\n');
-}
-
-
-function getListOfRooms(socket){
-      socket.write( "<= Active rooms are: \r\n");
-    for( var i=0; i < listOfRooms.length; i++){
-       socket.write(" *"+ listOfRooms[i] );
-       //TODO: calcualte count value here
-       var count = roomNameToAudienceSocketArrayMap[listOfRooms[i]].length;
-       socket.write("(" + count + ")\r\n");
-      
-    }
-    socket.write( "<= end of list. \r\n");
-}
-
-
-function joinARoom(socket, cleanData){
-    
-    var room = cleanData.substring(cleanData.indexOf(" ") + 1, cleanData.length);
-    console.log("Room Name = " + room + "\n");
-
-    // check if this room name is valid
-    if( -1 == listOfRooms.indexOf(room) ) {
-      //invalid room
-      // type error message and return
-      socket.write( " Room with this name doesn't exist. \r\n");
-      return;
-
-    }
-    // console.log("roomNameToAudienceSocketArrayMap= " + roomNameToAudienceSocketArrayMap + "\n");
-
-    roomNameToAudienceSocketArrayMap[room].push(socket);
-    user = getUserNameForSocket(socket);
-
-    if( typeof userNameToRoomNameMap[user]  !== 'undefined'){
-      var i =  roomNameToAudienceSocketArrayMap[userNameToRoomNameMap[user]].indexOf(socket);
-      if (i != -1) {
-        console.log("Removing user from room: "+ userNameToRoomNameMap[user]+ "\n");
-        roomNameToAudienceSocketArrayMap[userNameToRoomNameMap[user]].splice(i, 1);
-      }
-    }
-    socket.write( "Joined "  + room + " ! \r\n");
-
-    userNameToRoomNameMap[user] = room;
-    audience = roomNameToAudienceSocketArrayMap[room];
-
-    for(var i = 0; i<audience.length; i++) {
-      socket.write( '<= *' + getUserNameForSocket(audience[i]) );
-
-      if(socket === audience[i])
-        socket.write( '(** this is you) \n' );
-      else
-        socket.write( '\n' );     
-   }
-}
-
-
-
-
-function leaveARoom(socket){
-    var user =  getUserNameForSocket(socket);
-    var roomName = userNameToRoomNameMap[user];
-    userNameToRoomNameMap[user] = undefined;
-    socket.write( "Leaving " + roomName + "\r\n");
-    
-    var i =  roomNameToAudienceSocketArrayMap[roomName].indexOf(socket);
-    if (i != -1) {
-      console.log("Removing a user from the room. \n");
-      roomNameToAudienceSocketArrayMap[roomName].splice(i, 1);
-    }
-}
-
-
-
-
-function broadcaseMessage(socket, data){
-
-    // get room
-    // get list of users.
-
-    var user =  getUserNameForSocket(socket);
-    var roomName = userNameToRoomNameMap[user];
-     // if room is empty then broadcast
-
-
-    audience = roomNameToAudienceSocketArrayMap[roomName];
-    if( typeof audience === 'undefined'){
-      socket.write( '<=' +  getUserNameForSocket(socket) + ": ");
-      return;
-
-    }
-        
-    for(var i = 0; i<audience.length; i++) {
-
-      if (audience[i] !== socket) {
-        audience[i].write( "\r\n");
-        audience[i].write('=>' + user  + " : "+ data + "\n");
-        audience[i].write('<=' +  getUserNameForSocket(audience[i]) + ": ");
-      }
-    }
-    socket.write( '<=' +  getUserNameForSocket(socket) + ": ");
-}
-
-
-function assignUserName(socket, cleanData){
-      // this is a new socket
-
-    // is this a unique userName?
-    for (var key in socketNameToUserNameMap) {
-
-      if(socketNameToUserNameMap[key] == cleanData){
-        socket.write( "<= Sorry, name taken. \r\n<= Login Name? ");
-        return;
-      }
-    }
-    
-      socketNameToUserNameMap['mysocket.' + socketArray.indexOf(socket)] = cleanData;
-      socket.write( "<= Welcome " + cleanData + " ! \r\n");
-}
-
-
-
-
-
+// WHEN 2 people are typing, it cutsoff other person's messages.
 
 
 
@@ -182,6 +35,19 @@ function assignUserName(socket, cleanData){
   }
 })();
 
+
+
+function getUserNameForSocket(socket){
+    return socketNameToUserNameMap['mysocket.' + socket.myId] ;
+}
+
+2
+
+// Cleans the input of carriage return, newline
+function cleanInput(data) {
+  return data.toString().replace(/(\r\n|\n|\r)/gm,"");
+}
+ 
 
 
 
@@ -227,9 +93,6 @@ function receiveData(socket, data) {
 
 
 
-
-
-
 // When socker connection ends, remove socker from the array.
 function closeSocket(socket) {
   var i = socketArray.indexOf(socket);
@@ -245,6 +108,8 @@ function closeSocket(socket) {
 // Callback fucnction triggered when a new connection comes.
 function newSocket(socket) {
   
+
+  socket.myId = Math.floor((Math.random() * 1000000) + 1);;
   // Save in socker array
   socketArray.push(socket);
 
@@ -264,6 +129,180 @@ function newSocket(socket) {
   })
 }
  
+
+function showPrompt(socket){
+  socket.write( '<=' +  getUserNameForSocket(socket) + ": ");
+}
+
+
+
+
+
+
+
+//##################   ROOMS  ############################
+
+function getListOfRooms(socket){
+      socket.write( "<= Active rooms are: \r\n");
+    for( var i=0; i < listOfRooms.length; i++){
+       socket.write(" *"+ listOfRooms[i] );
+       //TODO: calcualte count value here
+       var count = roomNameToAudienceSocketArrayMap[listOfRooms[i]].length;
+       socket.write("(" + count + ")\r\n");
+      
+    }
+    socket.write( "<= end of list. \r\n");
+    showPrompt(socket);
+}
+
+
+
+//##################   JOIN A ROOM ############################
+
+function joinARoom(socket, cleanData){
+    
+    var room = cleanData.substring(cleanData.indexOf(" ") + 1, cleanData.length);
+    console.log("Room Name = " + room + "\n");
+
+    // check if this room name is valid
+    if( -1 == listOfRooms.indexOf(room) ) {
+      //invalid room
+      // type error message and return
+      socket.write( " Room with this name doesn't exist. \r\n");
+      return;
+
+    }
+    // console.log("roomNameToAudienceSocketArrayMap= " + roomNameToAudienceSocketArrayMap + "\n");
+
+    roomNameToAudienceSocketArrayMap[room].push(socket);
+    user = getUserNameForSocket(socket);
+
+    if( typeof userNameToRoomNameMap[user]  !== 'undefined'){
+      var i =  roomNameToAudienceSocketArrayMap[userNameToRoomNameMap[user]].indexOf(socket);
+      if (i != -1) {
+        console.log("Removing user from room: "+ userNameToRoomNameMap[user]+ "\n");
+        roomNameToAudienceSocketArrayMap[userNameToRoomNameMap[user]].splice(i, 1);
+      }
+    }
+    socket.write( "Joined "  + room + " ! \r\n");
+
+    userNameToRoomNameMap[user] = room;
+    audience = roomNameToAudienceSocketArrayMap[room];
+
+    for(var i = 0; i<audience.length; i++) {
+      socket.write( '<= *' + getUserNameForSocket(audience[i]) );
+
+      if(socket === audience[i])
+        socket.write( '(** this is you) \n' );
+      else
+        socket.write( '\n' );     
+   }
+
+   showPrompt(socket);
+}
+
+
+
+//##################   SEND MESSAGE  ############################
+
+function broadcaseMessage(socket, data){
+
+    // get room
+    // get list of users.
+
+    var user =  getUserNameForSocket(socket);
+    var roomName = userNameToRoomNameMap[user];
+     // if room is empty then broadcast
+
+
+    audience = roomNameToAudienceSocketArrayMap[roomName];
+    if( typeof audience === 'undefined'){
+      socket.write( '<=' +  getUserNameForSocket(socket) + ": ");
+      return;
+
+    }
+        
+    for(var i = 0; i<audience.length; i++) {
+
+      if (audience[i] !== socket) {
+        audience[i].write( "\r\n");
+        audience[i].write('=>' + user  + " : "+ data + "\n");
+        audience[i].write('<=' +  getUserNameForSocket(audience[i]) + ": ");
+      }
+    }
+    showPrompt(socket);
+}
+
+
+
+
+
+//##################   QUIT  ############################
+
+function quit(socket){
+  cleanDataStructures(socket);
+  socket.end('Goodbye!\n');
+}
+
+
+
+
+//##################   LEAVE A ROOM  ############################
+
+
+function cleanDataStructures(socket){
+    var user =  getUserNameForSocket(socket);
+    var roomName = userNameToRoomNameMap[user];
+    userNameToRoomNameMap[user] = undefined;
+
+    var i =  roomNameToAudienceSocketArrayMap[roomName].indexOf(socket);
+    if (i != -1) {
+      roomNameToAudienceSocketArrayMap[roomName].splice(i, 1);
+    }
+}
+
+function leaveARoom(socket){
+    var user =  getUserNameForSocket(socket);
+    var roomName = userNameToRoomNameMap[user];
+    if( typeof roomName === 'undefined'){
+      socket.write( "User is not in a chat room currently. \r\n");
+    }
+    else {
+      cleanDataStructures(socket);
+      socket.write( "Leaving " + roomName + "\r\n");
+    }
+    showPrompt(socket);
+}
+
+
+
+
+
+
+//##################   SET USERNAME  ############################
+
+function assignUserName(socket, cleanData){
+      // this is a new socket
+
+    // is this a unique userName?
+    for (var key in socketNameToUserNameMap) {
+
+      if(socketNameToUserNameMap[key] == cleanData){
+        socket.write( "<= Sorry, name taken. \r\n<= Login Name? ");
+        return;
+      }
+    }
+    
+      socketNameToUserNameMap['mysocket.' + socket.myId] = cleanData;
+      socket.write( "<= Welcome " + cleanData + " ! \r\n");
+      showPrompt(socket);
+}
+
+
+
+
+
+
 
 
 
